c93e9290070fad56afa13f15aeb3feb79004b929
pavelsumanoo <sumanoop@gmail.com>
2020-07-01 01:28:59 -0500

    request get y post

--- server.js
+++ server.js
@@ -1,10 +1,21 @@
 const express = require('express');
+const router = express.Router();
 
 var app = express();
 
-app.use('/', function (req, res) {
-  res.send('Start Server');
+app.use(router);
+
+router.get('/message', function (req, res) {
+  res.send('Hola desde get');
 });
+
+router.post('/message', function (req, res) {
+  res.send('Hola desde post');
+});
+
+// app.use('/', function (req, res) {
+//   res.send('Start Server');
+// });
 
 app.listen(3000);
 console.log('La aplicacion esta escuchando en http://localhost:3000');
